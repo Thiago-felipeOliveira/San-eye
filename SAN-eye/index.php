c87b734b004ae97df905a285d7cc3089fa816dc6
alice-lima-dev <125032253+alice-lima-dev@users.noreply.github.com>
2024-05-10 15:02:54 -0300

    primeiro arquivo

---
+++ SAN-eye/index.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="StyleSheet" href="style.css">
+    <title>SAN-Eye</title>
+</head>
+
+<body>
+
+    <nav class="header">
+        <div class="info">
+            <div class="logo-nav-box">
+                <img id="logo-nav" src="./img/logo.png">
+            </div>
+            <div class="nav-wrap">
+                <ul class="menu">
+                    <li><a href="index.php">Página Inicial</a></li>
+                    <li><a href="sobrenos.php">Clima</a></li>
+                    <li><a href="sobrenos.php">Aplicativo</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <div class="content">
+        <div class="content-block">
+            <img src="img/nuvem.png" alt="Logo HortaConnect" class="logo" id="panel-img">
+            <text class="panel" id="panel-text">"Antecipando o <span id="cor">futuro</span>, protegendo o <span
+                    id="cor">presente</span>.”</text>
+        </div>
+    </div>
+
+    <div class="container">
+        <h2>Previsões do tempo</h2>
+        <div class="box-1">
+
+            <div id="botoes">
+                <img id="img-clima" src="img/nuvem.png">
+                <div id="info">
+                    <p>11:00</p>
+                </div>
+            </div>
+
+            <p id="text-box"></p>
+        </div>
+    </div>
+
+
+</body>
+
+</html>
